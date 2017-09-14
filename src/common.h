9a7de1f2b7d58eb8c9b860d1e7505ba3f6ed1235
Daniel Lovasko <dlovasko@twosigma.com>
2017-09-14 01:20:29 +0100

    Incremented the minor version number.

--- src/common.h
+++ src/common.h
@@ -16,7 +16,7 @@
 
 // Semantic versioning scheme.
 #define MBEAT_VERSION_MAJOR 1
-#define MBEAT_VERSION_MINOR 4
+#define MBEAT_VERSION_MINOR 5
 #define MBEAT_VERSION_PATCH 0
 
 // Standard UDP port.
